9cb3676b8129147add4c049c026c67fdc8325591
angela chaj <angelachaj648@gmail.com>
2026-03-13 02:04:35 -0500

    califica

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+static void PromedioCalificaciones()
+{
+	double suma = 0;
+
+	for (int i = 1; i <= 3; i++)
+	{
+		Console.Write("Calificacion " + i + ": ");
+		suma += Double.Parse(Console.ReadLine());
+	}
+
+	Console.WriteLine("Promedio: " + (suma / 3));
+}
+
+
